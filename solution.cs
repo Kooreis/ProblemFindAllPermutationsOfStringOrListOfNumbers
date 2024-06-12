c4a5cf61075a7eae7233f967440507d72a60817f
Kooreis <kooreis.official@gmail.com>
2024-06-12 11:34:51 -0500

    In this update, the initial setup of the console application is done. The user is prompted to enter a string or list of numbers.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,3 @@
-Here is a simple console application in C# that finds all permutations of a string or list of numbers:
-
-```C#
 using System;
 using System.Collections.Generic;
 
@@ -10,35 +7,5 @@ class Program
     {
         Console.WriteLine("Enter a string or list of numbers:");
         string input = Console.ReadLine();
-        var permutations = GetPermutations(input);
-        foreach (var permutation in permutations)
-        {
-            Console.WriteLine(permutation);
-        }
-    }
-
-    static List<string> GetPermutations(string input)
-    {
-        var permutations = new List<string>();
-        if (input.Length == 1)
-        {
-            permutations.Add(input);
-        }
-        else
-        {
-            for (int i = 0; i < input.Length; i++)
-            {
-                string charAtI = input[i].ToString();
-                string remaining = input.Substring(0, i) + input.Substring(i + 1);
-                foreach (var permutation in GetPermutations(remaining))
-                {
-                    permutations.Add(charAtI + permutation);
-                }
-            }
-        }
-        return permutations;
     }
 }
-```
-
-This program prompts the user to enter a string or list of numbers. It then calculates all permutations of the input and prints them to the console. The `GetPermutations` method uses a recursive approach to generate all permutations.
