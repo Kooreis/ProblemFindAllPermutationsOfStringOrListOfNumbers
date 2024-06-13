b86c6158b1256dad977e1bf179cebb3329ae65f4
Kooreis <kooreis.official@gmail.com>
2024-06-13 12:34:51 -0500

    Defined a function to get all permutations of a string or a list of integers. If the input is neither a string nor a list of integers, the function returns an error message.

--- solution.py
+++ solution.py
@@ -1,1 +1,7 @@
-from itertools import permutations
+def get_permutations(input_data):
+    if isinstance(input_data, str):
+        return [''.join(p) for p in permutations(input_data)]
+    elif isinstance(input_data, list) and all(isinstance(i, int) for i in input_data):
+        return [list(p) for p in permutations(input_data)]
+    else:
+        return "Invalid input. Please enter a string or a list of integers."
