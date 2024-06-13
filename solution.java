6aac6c7df992f96fa176a6360136973a10a4b9e5
Kooreis <kooreis.official@gmail.com>
2024-06-13 12:34:51 -0500

    This is the permute method that recursively finds all permutations of the given string.

--- solution.java
+++ solution.java
@@ -1,11 +1,11 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a string:");
-        String str = scanner.nextLine();
-        int n = str.length();
-        Main permutation = new Main();
-        permutation.permute(str, 0, n - 1);
+private void permute(String str, int l, int r) {
+        if (l == r)
+            System.out.println(str);
+        else {
+            for (int i = l; i <= r; i++) {
+                str = swap(str, l, i);
+                permute(str, l + 1, r);
+                str = swap(str, l, i);
+            }
+        }
     }
